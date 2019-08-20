4adc68d09406093fdcf7ff0a3c09576482e731ac
berkererol <berkererol@gmail.com>
2019-08-20 22:49:13 +0000

    middle.js uploaded

---
+++ assertArraysEqual.js
@@ -0,0 +1,27 @@
+const eqArrays = function (array1, array2) {
+  if (array1.length !== array2.length) {
+    return false;
+  }
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i])
+      return false
+  }
+  return true
+}
+
+
+const assertArraysEqual = function (array1, array2) {
+  if (eqArrays(array1, array2) == true) {
+    console.log("The arrays are equal.")
+  } else {
+    console.log("The arrays are not equal.")
+  }
+}
+
+
+
+
+  assertArraysEqual([1, 2, 3], [1, 2, 3]) // => true
+  assertArraysEqual([1, 2, 3], [3, 2, 1]) // => false
+  assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]) // => true
+  assertArraysEqual(["1", "2", "3"], ["1", "2", 3]) // => false
