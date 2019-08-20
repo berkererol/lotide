8215c05a0ae84ef4e279d801f9f20fa8ca67146d
berkererol <berkererol@gmail.com>
2019-08-20 22:15:50 +0000

    revised and finalized

--- eqArrays.js
+++ eqArrays.js
@@ -6,9 +6,16 @@ const assertEqual = function (actual, expected) {
   }
 };
 
-
-
-
+const eqArrays = function (array1, array2) {
+  if (array1.length !== array2.length) {
+    return false;
+  }
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i])
+      return false
+  }
+  return true
+}
 
 
 eqArrays([1, 2, 3], [1, 2, 3]) // => true
@@ -16,3 +23,5 @@ eqArrays([1, 2, 3], [3, 2, 1]) // => false
 
 eqArrays(["1", "2", "3"], ["1", "2", "3"]) // => true
 eqArrays(["1", "2", "3"], ["1", "2", 3]) // => false
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
