514c6932db59f0e341a80f4c2c09c016bc24e83f
berkererol <berkererol@gmail.com>
2019-08-21 19:40:59 +0000

    Indexing letters in a string

--- letterPositions.js
+++ letterPositions.js
@@ -1,5 +1,14 @@
-const letterPositions = function(sentence) {
+const letterPositions = function (sentence) {
   const results = {};
-  // logic to update results here
-  return results;
+  for (let i = 0; i < sentence.length; i++) {
+    if (results[sentence[i]]) {
+      results[sentence[i]].push(i);
+    } else {
+      results[sentence[i]] = [i];
+    }
+  }
+  delete results[' ']
+  return results
 };
+
+console.log(letterPositions("BE   RKER"));
